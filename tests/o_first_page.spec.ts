dcc36e6b51d39c69797eb91f50eb2d9e525456a5
Cristea3051 <cristeavictor4@gmail.com>
2026-02-25 08:16:32 +0200

    second commit

--- tests/o_first_page.spec.ts
+++ tests/o_first_page.spec.ts
@@ -4,13 +4,11 @@ test('Homepage Orange.md heading', async ({ page }) => {
 
 await page.goto('https://www.orange.md/', { waitUntil: 'domcontentloaded' });
 
-  // Închide cookie banner-ul
 const agreeBtn = page.locator('#didomi-notice-agree-button');
 if (await agreeBtn.isVisible()) {
   await agreeBtn.click();
   await expect(agreeBtn).toBeHidden({ timeout: 5000 });
 }
 
-  // Verifică heading-ul "Orange"
 await expect(page).toHaveTitle(/Orange Moldova/);
 });
